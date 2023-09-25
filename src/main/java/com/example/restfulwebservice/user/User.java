e9f535507038b0ffa9c7ff378fb00ab30f8b20c3
Hyeyeon-Ko <khy33355@gmail.com>
2023-09-25 10:17:59 +0900

    URI를 이용한 API Version 관리

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -6,15 +6,18 @@ import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @JsonFilter("UserInfo")
 public class User {
 
     private Integer id;
+
     @Size(min=2, message = "Name은 2글자 이상 입력해 주세요.")
     private String name;
     @Past
