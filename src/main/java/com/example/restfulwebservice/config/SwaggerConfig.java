ef2980ba2a72fae66254977640202bafc40d33f4
Hyeyeon-Ko <khy33355@gmail.com>
2023-10-02 23:46:03 +0900

    swagger 사용

---
+++ src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.example.restfulwebservice.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@EnableSwagger2
+@OpenAPIDefinition
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI api(){
+        Info info = new Info().title("").version("v3").description("aaa");
+
+        return new OpenAPI().components(new Components()).info(info);
+    }
+
+}
