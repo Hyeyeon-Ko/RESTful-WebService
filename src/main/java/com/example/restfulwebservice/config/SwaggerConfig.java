8351db68c018c106089b6196ec7cfcfb03e4966d
Hyeyeon-Ko <khy33355@gmail.com>
2023-10-03 12:04:32 +0900

    Swagger Documentation 구현

--- src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
@@ -3,20 +3,69 @@ package com.example.restfulwebservice.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 //@EnableSwagger2
 @OpenAPIDefinition
 public class SwaggerConfig {
+//    강의 내용
+//    private static final Contact DEFAULT_CONTACT = new Contact("Kenneth Lee",
+//            "http://www.joneconsulting.co.kr", "edowon@joneconsulting.co.kr");
+//
+//    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome API Title",
+//            "My User management REST API service", "1.0", "urn:tos",
+//            DEFAULT_CONTACT, "Apache 2.0",
+//            "http://www.apache.org/license/LICENSE-2.0", new ArrayList<>());
+//
+//    커뮤니티 내용
+//    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(
+//            Arrays.asList("application/json", "application/xml"));
+
+//    private static final Contact CONTACT = new Contact().name("Kenneth Lee")
+//            .url("http://www.joneculsting.co.kr")
+//            .email("edowon@joneconsluting.co.kr");
+//    private static final License LICENSE= new License().name("Apache 2.0")
+//            .url("http://www.apache.org/licenses/LICENSE-2.0");
+//    private static final Info INFO = new Info().title("Awesome API Title")
+//            .contact(CONTACT)
+//            .description("Awesome API Documentation")
+//            .version("1.0")
+//            .license(LICENSE)
+//            .termsOfService("urn:tos");
+//    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES
+//            = new HashSet<>(Arrays.asList("application/json", "application/xml"));
 
     @Bean
-    public OpenAPI api(){
-        Info info = new Info().title("").version("v3").description("aaa");
+    public OpenAPI openAPI() {
+        Contact contact = new Contact()
+                .name("Kenneth Lee")
+                .url("http://www.joneculsting.co.kr")
+                .email("edowon@joneconsluting.co.kr");
 
-        return new OpenAPI().components(new Components()).info(info);
+        License license = new License()
+                .name("Apache 2.0")
+                .url("http://www.apache.org/licenses/LICENSE-2.0");
+
+        Info info = new Info()
+                .title("Awesome API Title")
+                .contact(contact)
+                .description("Awesome API Documentation")
+                .version("1.0")
+                .license(license)
+                .termsOfService("urn:tos");
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
     }
-
 }
--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -2,6 +2,7 @@ package com.example.restfulwebservice.user;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -14,15 +15,22 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 //@JsonFilter("UserInfo")
+@Schema(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
 
     private Integer id;
 
     @Size(min=2, message = "Name은 2글자 이상 입력해 주세요.")
+    @Schema(description = "사용자 이름을 입력해 주세요.")
     private String name;
+
     @Past
+    @Schema(description = "사용자 등록일을 입력해 주세요.")
     private Date joinDate;
 
+    @Schema(description = "사용자 패스워드를 입력해 주세요.")
     private String password;
+
+    @Schema(description = "사용자 주민번호를 입력해 주세요.")
     private String ssn;
 }
