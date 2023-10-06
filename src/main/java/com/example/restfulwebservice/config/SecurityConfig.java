e7ceade288e0dfa47b803e52b461a77eb55c916d
Hyeyeon-Ko <khy33355@gmail.com>
2023-10-06 11:02:09 +0900

    Configuration 클래스를 이용한 사용자 인증 처리

---
+++ src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.restfulwebservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+        throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("kenneth")
+                .password("{noop}test1234")
+                .roles("USER");
+    }
+}
