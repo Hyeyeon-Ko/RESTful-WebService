a5d31c9e6d1e6bea10b71ba261afda91998a22e6
Hyeyeon-Ko <khy33355@gmail.com>
2023-10-09 18:19:49 +0900

    JPA를 이용한 사용자 추가와 삭제 - POST/DELETE HTTP Method

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 insert into users
-values (1, current_timestamp(), 'User1', 'test1111', '701010-1111111');
+values (90001, current_timestamp(), 'User1', 'test1111', '701010-1111111');
 insert into users
-values (2, current_timestamp(), 'User2', 'test2222', '801010-1111111');
+values (90002, current_timestamp(), 'User2', 'test2222', '801010-1111111');
 insert into users
-values (3, current_timestamp(), 'User3', 'test3333', '901010-1111111');
+values (90003, current_timestamp(), 'User3', 'test3333', '901010-1111111');
